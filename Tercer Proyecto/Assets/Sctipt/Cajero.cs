fb750c4fd176010dbfcff6daed9056d17ed79df3
Julian-San-Pas <julian.sanchez_pas@uao.edu.co>
2025-09-03 17:55:38 -0500

    Primer avance
    xd

--- Tercer Proyecto/Assets/Sctipt/Cajero.cs
+++ Tercer Proyecto/Assets/Sctipt/Cajero.cs
@@ -1,42 +1,43 @@
-using JetBrains.Annotations;
+using packageCliente;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using packageCliente;
-using PackagePersona;
-using System.Collections;
-using System;
-public class Cajero : MonoBehaviour {
 
-    Queue<int> cajero = new Queue<int>();
+public class Cajero : MonoBehaviour
+{
+    Queue<Cliente> clientes = new Queue<Cliente>();
 
-    public TMP_InputField txtCorreo;
+    
+
     public TextMeshProUGUI texto;
-    int delayCoroutine;
+    public bool atendiendo = false;
 
-    public List<Cliente> clientes = new List<Cliente>();
 
-    public void Equeue()
+
+    public void Equeue(Cliente elCliente)
     {
-
+        clientes.Enqueue(elCliente);
     }
-    public void Dequeue()
+    public Cliente Dequeue()
     {
-        if (cajero.Count > 0)
+        if (clientes.Count > 0)
         {
-            int atendido = cajero.Dequeue();
-            texto.text = "Atendiendo al cliente con ID: " + atendido;
+            Cliente elCliente = clientes.Dequeue();
+            texto.text = "Atendiendo al cliente con ID: " + elCliente.IdCliente;
+            return elCliente;
         }
         else
         {
             texto.text = "No hay clientes en la fila.";
+            return null;
         }
     }
     public void MostrarClientes()
     {
-        if (cajero.Count > 0)
+        if (clientes.Count > 0)
         {
-            texto.text = "Clientes en la fila: " + string.Join(", ", cajero);
+            texto.text = "Clientes en la fila: " + string.Join(", ", clientes);
         }
         else
         {
@@ -45,70 +46,50 @@ public class Cajero : MonoBehaviour {
     }
     public void ClearQueue()
     {
-        cajero.Clear();
+        clientes.Clear();
         texto.text = "La fila quedo limpia.";
     }
-    public void AddClient()
+    public void ActivarCajero()
     {
-        /*if (int.TryParse(txtCorreo.text, out int clientId))
+        for (int i = 0; i < clientes.Count; i++)
         {
-            cajero.Enqueue(clientId);
-            texto.text = "Cliente con ID: " + clientId + " añadido a la fila.";
-            txtCorreo.text = "";
-        }*/
-
-        if (!int.IsNullOrEmpty(txtCorreo.text))
-        {
-
-            int idCliente = Generador().ToString();
-            cajero.Enqueue(idCliente);
-            texto.text = "Cliente con ID: " + idCliente + " añadido a la fila.";
-            //txtCorreo.text = "";
-        }
-        else
-        {
-            texto.text = "Por favor, ingrese un ID de cliente válido.";
-        }
-    }
-    public void RemoveClient() 
-    { 
-    }
-
-    public void clienteAleatorio()
-    {
+            StartCoroutine(Activar());
+            texto.text = "Clientes en la fila: " + string.Join(", ", clientes);
+        }   
         
-        string[] listaNombres = { "Juan", "María", "Pedro", "Ana", "Luis", "Pepe", "Mani", "Rosa", "Betty", "Jose" };
-        string[] listaCorreos = { "juan@gmail.com", "mari@uao.edu.co", "pe@hotmail.com", "an@yahoo.com", "lui@gmail.com.co", "pep@gmail.com", "mani@gmail.com", "Ros@gmail.com", "bet@gmail.com", "jos@gmail.com" };
-        string[] listaDirecciones = { "Calle 1", "Carrera 2", "Avenida 3", "Calle 4", "Carrera 5" };
-        string[] listaTramites = { "retirar", "consignar" };
-        int idCliente = UnityEngine.Random.Range(1000, 9999);
-        int segundos = UnityEngine.Random.Range(1, 6);
-
-        int indexNomCor = UnityEngine.Random.Range(0, listaNombres.Length);
-
-        string nombre = listaNombres[indexNomCor];
-        string correo = listaCorreos[indexNomCor];
-        string direccion = listaDirecciones[UnityEngine.Random.Range(0, listaDirecciones.Length)];
-        string tramite = listaTramites[UnityEngine.Random.Range(0, listaTramites.Length)];
-        Cliente elCliente = new Cliente(idCliente.ToString(), tramite, segundos, nombre, correo, direccion);
-        clientes.Add(elCliente);
     }
-
-    //==START==//
+    public void DeactivarCajero()
+    {
+        StopCoroutine(Activar());
+        atendiendo = false;
+        texto.text = "Cajero desactivado.";
+    }
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(Generador());
+        
     }
 
-    //==UPDATE==//
+    // Update is called once per frame
     void Update()
     {
-        //Public clienteAleatorio();
+        
     }
-    IEnumerator Generador()
+    IEnumerator Activar()
     {
-        delayCoroutine = UnityEngine.Random.Range(1, 4);
-        clienteAleatorio();
-        yield return new WaitForSeconds(delayCoroutine);
+        Cliente elCliente = Dequeue();
+        if (elCliente != null)
+        {
+            atendiendo = true;
+            yield return new WaitForSeconds(elCliente.AtencionT);
+            atendiendo = false;
+            texto.text = "El cliente con ID: " + elCliente.IdCliente + " ha sido atendido.";
+        }
+
+        else
+        {
+            texto.text = "No hay clientes para atender.";
+        }
     }
 }
